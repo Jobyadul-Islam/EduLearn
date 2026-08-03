afba03fcca7ebd815bcfe17362ab61c29c73b6bd
Md. Jobyadul Islam <ratulkhan22555@gmail.com>
2026-08-04 01:18:32 +0600

    Initial EduLearn project setup

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using EduLearn.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EduLearn.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
