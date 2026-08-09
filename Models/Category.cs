fdb6b6279695599a5fdf9cb4f1a417df23656b3d
Md. Jobyadul Islam <ratulkhan22555@gmail.com>
2026-08-09 21:27:55 +0600

    Add role-based authorization, Admin dashboard, and Category CRUD

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+using System.Collections.Generic;
 
 namespace EduLearn.Models
 {
@@ -7,6 +10,8 @@ namespace EduLearn.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+
+        [ValidateNever]
         public ICollection<Course> Courses { get; set; }
     }
 }
